1b901ae0ec15d961d38db6e89bf133e85edd77dd
Vlad Stirbu <vstirbu@gmail.com>
2018-01-07 23:55:55 +0200

    scale diagram to fit preview window (fixed #2)

--- viewer/renderer.js
+++ viewer/renderer.js
@@ -1,5 +1,4 @@
 function renderer(spec) {
-  console.log(spec);
   // Create a new directed graph
   const g = new dagreD3.graphlib.Graph().setGraph({});
 
@@ -29,12 +28,15 @@ function renderer(spec) {
         inner.attr('transform', d3.event.transform);
       });
   svg.call(zoom);
-  svg.attr('height', window.innerHeight);
-  svg.attr('width', window.innerWidth - 10);
+
+  // Fit svg to preview window
+  svg.attr('height', window.innerHeight - 5);
+  svg.attr('width', window.innerWidth - 5);
 
   // Create the renderer
   const render = new dagreD3.render();
 
+  // Custom shapes
   render.shapes().initial = initial;
   render.shapes().final = final;
   render.shapes().state = state;
@@ -43,12 +45,26 @@ function renderer(spec) {
   // Run the renderer. This is what draws the final graph.
   render(inner, g);
 
+  // Determine scaling factor
+  const initialScale = scalingFactor(g, svg);
+
   // Center the graph
-  const initialScale = 1;
   const width = (svg.attr('width') - g.graph().width * initialScale) / 2;
   const height = (svg.attr('height') - g.graph().height * initialScale) / 2;
 
   svg.call(zoom.transform, d3.zoomIdentity.translate(width, height).scale(initialScale));
+}
+
+function scalingFactor(g, svg) {
+  const graphHeight = g.graph().height;
+  const svgHeight = svg.attr('height');
+  const verticalScale = graphHeight > svgHeight ? svgHeight / (graphHeight + 20) : 1;
+
+  const graphWidth = g.graph().width;
+  const svgWidth = svg.attr('width');
+  const horizontalScale = graphWidth > svgWidth ? svgWidth / (graphWidth + 20) : 1;
+
+  return Math.min(horizontalScale, verticalScale);
 }
 
 function initial(parent, bbox, node) {
