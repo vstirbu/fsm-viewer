1fa8bedbf01ff5c9d2cc683a887d254ce61f7ddf
Vlad Stirbu <vstirbu@gmail.com>
2018-11-27 17:34:03 +0200

    added test example

---
+++ test/example.js
@@ -0,0 +1,14 @@
+const fsm = require('fsm-as-promised');
+
+fsm({
+  initial: 'new',
+  final: ['end'],
+  events: [
+    { name: 'start', from: 'new', to: 'round' },
+    { name: 'end', from: 'round', to: ['extratime', 'end'], condition: () => {}},
+    { name: 'end', from: 'extratime', to: 'end' }
+  ],
+  callbacks: {
+    onenterround: () => {}
+  }
+});
